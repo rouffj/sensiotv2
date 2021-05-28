3c4bf9f896dc54f567fc7d349d09fc6f0876b4fe
Joseph Rouff <rouffj@gmail.com>
2021-05-28 11:22:54 +0200

    make:auth generated

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\Movie;
 use App\Entity\User;
 use App\Entity\Review;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
@@ -43,11 +44,11 @@ class UserController extends AbstractController
     /**
      * @Route("/login.html", name="login")
      */
-    public function login(): Response
-    {
-        return $this->render('user/login.html.twig', [
-        ]);
-    }
+    //public function login(): Response
+    //{
+    //    return $this->render('user/login.html.twig', [
+    //    ]);
+    //}
 
     /**
      * @Route("/user-cart.html", name="cart")
@@ -110,6 +111,31 @@ class UserController extends AbstractController
     {
         return $this->render('user/user-watchlist.html.twig', [
         ]);
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
 }
